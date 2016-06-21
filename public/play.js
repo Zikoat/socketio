71b358ab44f931909766e4d1cd591749b70332c1
sigurd-laptop <sschoeler99@gmail.com>
2016-06-21 17:22:15 +0200

    created class Player, doesnt do anything yet

--- public/play.js
+++ public/play.js
@@ -1,9 +1,10 @@
 //strict should not be used
+/*jshint esversion: 6 */ 
 var socket = io();
 
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update, render:render });
 
-var platforms, player, cursors, stars, score = 0, scoreText, globalScoreText = 0, controlState;
+let platforms, player, cursors, stars, score = 0, scoreText, globalScoreText = 0, controlState;
 
 function preload() {
     game.load.image('sky', 'assets/sky.png');
@@ -12,6 +13,16 @@ function preload() {
     game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
     game.load.spritesheet('baddie', 'assets/baddie.png', 32, 32);
     game.load.image('ship', 'assets/ship.png');
+}
+
+class Player extends Phaser.Sprite {
+	constructor(x=50, y=50){
+		super(game, x, y, "ship");
+	}
+
+	movement () {
+
+	}
 }
 
 function create() {
@@ -54,7 +65,11 @@ function update() {
 		scoreText.text = "Score: " + score;
 		star.body.bounce.y =0.7 + Math.random() * 0.2;
 	}
+	playerMovement.rotate();
+}
 
+
+function playerMovement(controlState) {
 	// movement--------
 	if(controlState == "4-way"){
 		if (cursors.left.isDown) {
@@ -75,15 +90,24 @@ function update() {
 
 		player.body.velocity.x = game.math.clamp(player.body.velocity.x, -400, 400);
 		player.body.velocity.y = game.math.clamp(player.body.velocity.y, -400, 400);
-	} else if (controlState == "rotate") {
-		if(cursors.left.isDown){player.rotation -= 0.05;}
-		if(cursors.right.isDown){player.rotation += 0.05;}
-		if(cursors.up.isDown){
-			player.body.acceleration = game.physics.arcade.accelerationFromRotation(player.rotation, 200);
-
-		} else {player.body.acceleration = 0;}
-		player.body.velocity.clamp(-200,200);
 	}
+}
+
+playerMovement.rotate = function () {
+	if(cursors.left.isDown){player.rotation -= 0.05;}
+	if(cursors.right.isDown){player.rotation += 0.05;}
+	if(cursors.up.isDown){
+		player.body.acceleration = game.physics.arcade.accelerationFromRotation(player.rotation, 200);
+	} else {player.body.acceleration = 0;}
+	player.body.velocity.clamp(-200,200);
+};
+
+function horizontalAxis(left, right) {
+	
+}
+
+function verticalAxis() {
+
 }
 
 function render() {
@@ -92,9 +116,11 @@ function render() {
 	// game.debug.text(player.body.acceleration, 20, 20);
 
 }
+
 window.addEventListener('load', function() {
     new FastClick(document.body);
 }, false);
+
 
 
 socket.on("increase", function(count){
